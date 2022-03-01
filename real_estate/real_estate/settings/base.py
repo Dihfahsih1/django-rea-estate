f74c1ae41a19c7f9bcf4a8e21490f40687caa620
Dihfahsih1 <dihfahsihm@gmail.com>
2022-03-01 20:51:05 +0300

    work on the loggers

--- real_estate/real_estate/settings/base.py
+++ real_estate/real_estate/settings/base.py
@@ -136,6 +136,24 @@ import logging
 import logging.config
 from django.utils.log import DEFAULT_LOGGING
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(__name__)#create logger instance
 
-LOG_LEVEL='log level'
+LOG_LEVEL='INFO'
+
+logging.config.dictConfig({
+    "version":1,
+    "disable_existing_loggers":False,
+    "formatters":{
+        "console":{
+            "format":"%(asctime)s %(name)-12s %(levelname)-8s %(message)s"
+        },
+        "file":{"format":"%(asctime)s %(name)-12s %(levelname)-8s %(message)s"},
+        "django.server":DEFAULT_LOGGING["formatters"]["django.server"]
+    },
+    "handlers":{
+        "console":{
+            "class":"logging.StreamHandler",
+            "formatter":"console",
+        }
+    }
+})
