ee0db99fbd2f5b26d75ea20771a3d3de8477ac73
Dihfahsih1 <dihfahsihm@gmail.com>
2022-03-09 23:55:27 +0300

    add more fields to serializer

--- real_estate/apps/users/serializers.py
+++ real_estate/apps/users/serializers.py
@@ -1,2 +1,20 @@
 from django.contrib.auth import get_user_model
+from numpy import source
 from django_countries.serializer_fields import CountryField
+from djoser.serializers import UserCreationSerializer
+from phonenumber_field.serializersfields import PhoneNumberField
+from rest_framework import serializers
+
+User = get_user_model
+
+class UserSerializer(serializers.ModelSerializer):
+  gender = serializers.CharField(source="profile.gender")
+  phone_number= PhoneNumberField(source="profile.phone_number")
+  profile_photo = serializers.ImageField(source="profile.profile_photo")
+  country = CountryField(source="profile.country",)
+  city = serializers.CharField(source="profile.city")
+  top_seller = serializers.BooleanField(source="profile.top_seller")
+  first_name = serializers.SerializerMethodField()
+  last_name = serializers.SerializerMethodField()
+  full_name = serializers.SerializerMethodField(source="get_full_name")
+  
