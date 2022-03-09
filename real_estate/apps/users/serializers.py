bb79813306c2f3558a97c8cb1ab72d6ba146a418
Dihfahsih1 <dihfahsihm@gmail.com>
2022-03-10 00:16:06 +0300

    finish the serializer

--- real_estate/apps/users/serializers.py
+++ real_estate/apps/users/serializers.py
@@ -1,7 +1,8 @@
+from dataclasses import fields
 from django.contrib.auth import get_user_model
 from numpy import source
 from django_countries.serializer_fields import CountryField
-from djoser.serializers import UserCreationSerializer
+from djoser.serializers import UserCreateSerializer
 from phonenumber_field.serializersfields import PhoneNumberField
 from rest_framework import serializers
 
@@ -20,5 +21,23 @@ class UserSerializer(serializers.ModelSerializer):
   
   class Meta:
     model=User  
-    fields = ["id", "username", "email", "first_name", "last_name", "full_name"]
+    fields = ["id", "username", "email", "first_name", "last_name", "full_name", "gender", "phone_number", "profile_photo", "country","city", "top_seller"]
   
+  def get_first_name(self, obj):
+    return obj.first_name.title()
+  
+  def get_last_name(self, obj):
+    return obj.last_name.title()
+  
+  def to_representation(self, instance):
+    representation = super(UserSerializer, self).to_representation(instance)
+    if instance.is_superuser:
+      representation["admin"]=True
+      
+    return representation
+      
+class CreateUserSerializer(UserCreateSerializer):
+  class Meta(UserCreateSerializer.Meta):
+    model=User
+    fields = ["id", "username", "email", "first_name", "last_name","password"]
+         
