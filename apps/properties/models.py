a92aab3313eb17fb1f463cb3adc0fdf895487495
Dihfahsih1 <dihfahsihm@gmail.com>
2022-06-04 23:28:56 +0300

    fix the errors

--- apps/properties/models.py
+++ apps/properties/models.py
@@ -8,10 +8,10 @@ from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
-from django_countries import CountryField
+from django_countries.fields import CountryField
 from apps.common.models import TimeStampedUUIDModel
 
-User = get_user_model
+User = get_user_model()
 
 class PropertyPublishedManager(models.Manager):
   def get_queryset(self):
@@ -35,8 +35,8 @@ class Property(TimeStampedUUIDModel):
   user = models.ForeignKey(User, verbose_name="Aent, Seller, Buyer", related_name="agent_buyer", on_delete=models.DO_NOTHING)
   title = models.CharField(max_length=250, verbose_name=_("Property Title"))
   slug=AutoSlugField(populate_from="title", unique=True, always_update=True)
-  ref_code = models.CharField(verbose_name=_("Property Reference code", max_length=225, unique=True, blank=True))
-  description=models.TextField(default=_("Default Description"), default=_("Default description...update me please ..."))
+  ref_code = models.CharField(verbose_name=_("Property Reference code"), max_length=225, unique=True, blank=True)
+  description=models.TextField(verbose_name=_("Default Description"), default=_("Default description...update me please ..."))
   country=CountryField(verbose_name=_("country"), default="UG", blank_label="(select Country")
   
   
